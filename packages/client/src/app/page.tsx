65cd1484189f1599c42e9c9348541fa7da95fce6
bibitto <bibitto.crypto@gmail.com>
2024-03-02 13:12:19 +0900

    add wallet-actions sample components

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Card, CardProps } from '@/components/Card'
-import { Cards } from '@/components/Cards';
-import { Header } from '@/components/Header'
+import { Card, CardProps } from "@/components/Card";
+import { Cards } from "@/components/Cards";
+import { Header } from "@/components/Header";
+import { WalletActionsMock } from "@/components/WalletActionsMock";
 
 export default function Home() {
-
   const markets: CardProps[] = [
     {
       imageUrl: "https://source.unsplash.com/random/800x600",
@@ -25,14 +25,17 @@ export default function Home() {
     },
   ];
 
-  
   return (
     <div>
       <Header />
-      <div className='p-5'>
-        <h1 className='text-3xl font-bold'>Featured Markets</h1>
+      <div className="p-5 flex flex-col items-center">
+        <h1 className="text-3xl font-bold">Wallet Actions</h1>
+        <WalletActionsMock />
+      </div>
+      <div className="p-5 flex flex-col items-center">
+        <h1 className="text-3xl font-bold">Featured Markets</h1>
       </div>
       <Cards cards={markets} />
     </div>
-  )
+  );
 }
